0f453c0d48fe7c0625255ac1beffa27fb10a12ff
MAlngaawy <m.alngaawy@gmail.com>
2022-09-01 13:35:18 +0200

    initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container mx-auto w-4/5">
+        <h2 className='text-2xl text-center my-10 font-bold'>
+          Hello This is a rect app to test any new technonogies
+        </h2>
+      </div>
     </div>
   );
 }
