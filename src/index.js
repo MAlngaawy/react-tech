376ba8473cd7da89bf3098700a9ed78fc635894b
MAlngaawy <m.alngaawy@gmail.com>
2022-09-06 13:51:42 +0200

    access data from the slice

--- src/index.js
+++ src/index.js
@@ -11,8 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}></Provider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const Home = () => {
+  const { amount } = useSelector((store) => store.cart);
+  console.log(amount);
+
   return (
     <div className="container mx-auto w-4/5">
       <h2 className="text-2xl text-center py-6 font-bold">
         Hello This is a rect app to test any new technonogies
+        {amount}
       </h2>
       <Link to="/form">go to from Page</Link>
     </div>
