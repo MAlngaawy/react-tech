9f05630f2053773c7e00ab2fe57e6409cf251329
MAlngaawy <m.alngaawy@gmail.com>
2022-09-06 13:36:40 +0200

    init thje cart state by redux toolkit

--- src/store.jsx
+++ src/store.jsx
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
+import cartReducer from "./features/cart/cartSlice";
 
-export default configureStore({
+export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    cart: cartReducer,
   },
 });
